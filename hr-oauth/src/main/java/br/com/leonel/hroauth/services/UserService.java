e65c0f763a3db8ebd53748da718609a746bed08f
Lonel de Sousa <leonel55sousa@gmail.com>
2022-09-14 15:15:51 -0300

    feat: Login, JWT token

--- hr-oauth/src/main/java/br/com/leonel/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/leonel/hroauth/services/UserService.java
@@ -1,6 +1,9 @@
 package br.com.leonel.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.leonel.hroauth.entities.User;
@@ -9,19 +12,31 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
     public User findByEmail(String email) {
         User user = userFeignClient.findByEmail(email).getBody();
-        if (user == null){
+        if (user == null) {
             log.error("Email not found: {}", email);
             throw new IllegalArgumentException("Email not found");
         }
 
         log.info("Email found: {}", email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            log.error("Email not found: {}", username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        log.info("Email found: {}", username);
         return user;
     }
 }
